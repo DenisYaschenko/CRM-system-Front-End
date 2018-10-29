5e259190b6f23987cd3137e2f61c3db41ae31146
Denis Yaschenko <deniskrut3233@gmail.com>
2018-10-29 13:37:59 +0200

    create materialize toasts

--- src/app/login-page/login-page.component.ts
+++ src/app/login-page/login-page.component.ts
@@ -3,6 +3,7 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { AuthService } from '../shared/services/auth.service';
 import { Subscription } from 'rxjs';
 import { Router, ActivatedRoute, Params } from '@angular/router';
+import { MaterialService } from '../shared/classes/material.service';
 
 @Component({
   selector: 'app-login-page',
@@ -26,8 +27,10 @@ export class LoginPageComponent implements OnInit, OnDestroy {
 
     this.route.queryParams.subscribe((params: Params) => {
       if (params['registered']) {
+        MaterialService.toast('Now you can login in system');
         // now you can login in system
       } else if (params['accessDenied']) {
+        MaterialService.toast('For start autorizate in system');
         // for start autorizate in system
       }
     });
@@ -44,7 +47,7 @@ export class LoginPageComponent implements OnInit, OnDestroy {
     this.aSub = this.auth.login(this.form.value).subscribe(
       () => this.router.navigate(['/overview']),
       error => {
-        console.warn(error);
+        MaterialService.toast(error.error.message);
         this.form.enable();
       }
     );
