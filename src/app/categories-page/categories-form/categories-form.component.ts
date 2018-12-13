6c7b7a36fb12d0150ac203be230295f6d48a6c7f
Denis Yaschenko <denis1999yaschenko@gmail.com>
2018-12-13 15:35:25 +0200

    Delete category

--- src/app/categories-page/categories-form/categories-form.component.ts
+++ src/app/categories-page/categories-form/categories-form.component.ts
@@ -1,5 +1,5 @@
-import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { CategoriesService } from 'src/app/shared/services/categories.service';
 import { switchMap } from 'rxjs/operators';
@@ -21,7 +21,8 @@ export class CategoriesFormComponent implements OnInit {
   category: Category;
 
   constructor(private route: ActivatedRoute,
-              private categoriesService: CategoriesService) { }
+              private categoriesService: CategoriesService,
+              private router: Router) { }
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -81,6 +82,18 @@ export class CategoriesFormComponent implements OnInit {
 
   triggerClick() {
     this.inputRef.nativeElement.click();
+  }
+
+  deleteCategory() {
+    const decision = window.confirm(`Вы уверены, что хотите удалить категорию "${this.category.name}"?`);
+    if (decision) {
+      this.categoriesService.delete(this.category._id)
+        .subscribe(
+          response => MaterialService.toast(response.message),
+          error => MaterialService.toast(error.error.message),
+          () => this.router.navigate(['/categories'])
+        );
+    }
   }
 
   onFileUpload(event: any) {
--- src/app/shared/services/categories.service.ts
+++ src/app/shared/services/categories.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Category } from '../interfaces';
 import { Observable } from 'rxjs';
+import { Message } from '../interfaces';
 
 @Injectable({
     providedIn: 'root'
@@ -35,5 +36,9 @@ export class CategoriesService {
       }
       fd.append('name', name);
       return this.http.patch<Category>(`/api/category/${id}`, fd);
+    }
+
+    delete(id: string): Observable<Message> {
+      return this.http.delete<Message>(`/api/category/${id}`);
     }
 }
