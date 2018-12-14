67dcc2a0c729f4c00f8a8fed586bb802ab173179
Denis Yaschenko <denis1999yaschenko@gmail.com>
2018-12-14 16:42:23 +0200

    work with categories 51

--- src/app/categories-page/categories-form/position-form/position-form.component.ts
+++ src/app/categories-page/categories-form/position-form/position-form.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { PositionService } from 'src/app/shared/services/positions.service';
+import { Position } from '../../../shared/interfaces';
 
 @Component({
   selector: 'app-position-form',
@@ -6,10 +8,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./position-form.component.css']
 })
 export class PositionFormComponent implements OnInit {
+   
+  @Input('categoryId') categoryId: string;
+  positions: Position[] = [];
+  loading = false;
 
-  constructor() { }
+  constructor(private positionService: PositionService) { }
 
   ngOnInit() {
+    this.loading = true;
+    this.positionService.fetch(this.categoryId).subscribe(positions => {
+      this.positions = positions;
+      this.loading = false;
+    });
   }
 
 }
