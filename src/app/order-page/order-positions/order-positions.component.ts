6c34800f3084bb1f0cd6035e35cb674ca7e10575
Denis Yaschenko <denis1999yaschenko@gmail.com>
2018-12-28 13:33:04 +0200

    modal window, navigation in positions, list of products

--- src/app/order-page/order-positions/order-positions.component.ts
+++ src/app/order-page/order-positions/order-positions.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { PositionService } from 'src/app/shared/services/positions.service';
+import { Observable } from 'rxjs';
+import { Position } from 'src/app/shared/interfaces';
+import { switchMap, map } from 'rxjs/operators';
+import { OrderService } from '../order.service';
 
 @Component({
   selector: 'app-order-positions',
@@ -7,9 +13,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrderPositionsComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('input') input: ElementRef;
+
+  positions$: Observable <Position[]>
+  validNumber: boolean = false;
+
+    constructor(private route: ActivatedRoute,
+      private positionsService: PositionService,
+      private order: OrderService) {}
 
   ngOnInit() {
+    this.positions$ = this.route.params
+      .pipe(switchMap(
+        (params: Params) => {
+          return this.positionsService.fetch(params['id'])
+        }
+      ),
+        map(
+          (positions: Position[]) => {
+            return positions.map(position => {
+              position.quantity = 1;
+              return position;
+            })
+          }
+        ))
+  }
+
+  someChanges($event) {
+    this.validNumber = $event > 0 ? false : true;
+
+  }
+
+  addToOrder(position: Position) {
+    
+    this.order.add(position);
   }
 
 }
